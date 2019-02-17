8f491ef58671b25ac99a39d8ab16fdc5e1e5be13
ernut <43087942+ernut@users.noreply.github.com>
2019-02-17 15:53:13 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,14 +4,19 @@ echo "# The termux repository mirror from TUNA:
 deb http://mirrors.tuna.tsinghua.edu.cn/termux stable main" >$PREFIX/etc/apt/sources.list
 
 #trojan
-pkg install git clang cmake boost-dev openssl-dev golang wget -y
+pkg install git clang cmake boost-dev openssl-dev -y
 git clone https://github.com/trojan-gfw/trojan
-cd trojan/
+cd trojan
 sed -i '/if(WIN32)/i\target_link_libraries(trojan atomic)' CMakeLists.txt
 cmake -DCMAKE_INSTALL_PREFIX=/data/data/com.termux/files/usr -DENABLE_MYSQL=OFF .
 make install
 
+#trojan配置
+cp -f /data/data/com.termux/files/usr/share/doc/trojan/examples/client.json-example /data/data/com.termux/files/usr/etc/trojan/config.json
+read -p "输入trojan服务器域名:" domain && read -p "输入trojan密码:" password && sed -i "s/password1/$password/g;s/example.com/$domain/g" /data/data/com.termux/files/usr/etc/trojan/config.json
+
 #clash
+pkg install golang wget -y
 cd ~
 go get -u -ldflags "-s -w" github.com/Dreamacro/clash && chmod +x go/bin/clash && mv go/bin/clash /data/data/com.termux/files/usr/bin/
 
@@ -41,7 +46,3 @@ trojan & calsh &" >> ~/.termux/boot/trojan
 #清理
 #pkg uninstall clang cmake boost-dev golang -y
 #apt autoremove -y
-
-#trojan配置
-cp -f /data/data/com.termux/files/usr/share/doc/trojan/examples/client.json-example /data/data/com.termux/files/usr/etc/trojan/config.json
-read -p "输入trojan服务器域名:" domain && read -p "输入trojan密码:" password && sed -i "s/password1/$password/g;s/example.com/$domain/g" /data/data/com.termux/files/usr/etc/trojan/config.json
